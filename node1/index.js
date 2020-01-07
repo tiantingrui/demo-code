19245767613674d981beb9e3d699a59285160c15
terry <976912095@qq.com>
2020-01-07 23:17:20 +0800

    'node1'

---
+++ node1/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+
+// 理解node 的一个特点 非阻塞 IO
+fs.readFile('./data/name.json', (err, data) => {
+    if (!err) {
+        setTimeout(() => {
+            console.log('我三秒后执行')
+        },3000)
+        console.log(data.toString())
+    }
+})
+console.log('我是最后才执行')
